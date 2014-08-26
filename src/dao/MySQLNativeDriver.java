3d6fce7adad386eb3d9ca6b435b2219bbc2256fb
matrix17 <luiz_mx@live.com>
2014-08-26 09:18:22 -0300

    metodos da classe de conexao com o banco adicionado.

--- src/dao/MySQLNativeDriver.java
+++ src/dao/MySQLNativeDriver.java
@@ -1,6 +1,5 @@
 package dao;
 
-
 /**
  * Classe responsável por efetuar a conexão com o banco
  *
@@ -8,5 +7,47 @@ package dao;
  */
 public class MySQLNativeDriver {
 
-   
-}
+    protected String nomeBanco, usuario, senha;
+    private String ip, porta;
+
+    private void setValues(String nomeBanco, String usuario,
+            String senha, String ip, String porta) {
+        this.nomeBanco = nomeBanco;
+        this.usuario = usuario;
+        this.senha = senha;
+        this.ip = ip;
+        this.porta = porta;
+    }
+
+    public MySQLNativeDriver(String nomeBanco, String usuario, String senha, String ip, String porta) {
+        this.setValues(nomeBanco, usuario, senha, ip, porta);
+    }
+
+    public MySQLNativeDriver(String nomeBanco, String usuario, String senha) {
+        this.setValues(nomeBanco, usuario, senha,
+                "localhost", "3306");
+    }
+
+    public Connection obterConexao() throws SQLException {
+        Connection conexao = null;
+
+        try {
+            String url = "jdbc:mysql://" + this.ip + ":" + this.porta
+                    + "/" + this.nomeBanco;
+            Class.forName("com.mysql.jdbc.Driver");
+            conexao = DriverManager.getConnection(url,
+                    this.usuario, this.senha);
+        } catch (ClassNotFoundException erro) {
+            System.out.println("Driver MySQL não encontrado");
+        } catch (SQLException erro) {
+            System.out.println(
+            "Problemas na conexão com 
+banco de dados: "
+                    + erro.getMessage()
+          
+            );
+        } finally {
+            return conexao;
+        }
+
+    }
