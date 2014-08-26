ef26ba49685719a0f3c450d1a76a22b168f36609
matrix17 <luiz_mx@live.com>
2014-08-26 09:09:21 -0300

    Escopo das classes da dao criadas.

---
+++ src/dao/Armazena.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+
+/**
+ *Classe responsável pela persistência no banco, utilizando jdbc
+ * 
+ * @author Matheus Claudino
+ */
+public class Armazena {
+/**/
+    
+    
+   
+}
---
+++ src/dao/MySQLNativeDriver.java
@@ -0,0 +1,12 @@
+package dao;
+
+
+/**
+ * Classe responsável por efetuar a conexão com o banco
+ *
+ * @author Matheus Claudino
+ */
+public class MySQLNativeDriver {
+
+   
+}
