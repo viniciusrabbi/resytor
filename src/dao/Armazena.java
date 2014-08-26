3c8ecdb3f3c98f39582723a4552ce5e784bca35b
matrix17 <luiz_mx@live.com>
2014-08-26 09:28:57 -0300

    metodo de insercao da mensagem adicionado a classe responsavel pela persistencia

--- src/dao/Armazena.java
+++ src/dao/Armazena.java
@@ -4,15 +4,40 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
- *Classe responsável pela persistência no banco, utilizando jdbc
- * 
+ * Classe responsável pela persistência no banco, utilizando jdbc
+ *
  * @author Matheus Claudino
  */
 public class Armazena {
-/**/
-    
-    
-   
+
+    /**
+     *
+     * Método para a inserção da mensagem no banco
+     *
+     * @param text mensagem recebida
+     * @throws java.sql.SQLException
+     */
+    public void insert(String text) throws SQLException {
+        Connection conexao;
+        MySQLNativeDriver driver = new MySQLNativeDriver("BD_resytor", "root", "");
+        conexao = driver.obterConexao();
+
+        String sql = "INSERT INTO mensagem(conteudo)VALUES(?);";
+
+        PreparedStatement stmt;
+
+        try {
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, text);
+            stmt.executeUpdate();
+        } catch (SQLException erro) {
+            System.out.println("Erro na inserção (" + erro.getLocalizedMessage() + ")");
+        }
+    }
+
 }
