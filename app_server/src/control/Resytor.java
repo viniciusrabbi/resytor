5204128df6c892410910af45196cce482fbfb82a
luizventurote <luiz_17@ymail.com>
2014-08-29 10:32:17 -0300

    Classe Resytor adicionada

---
+++ app_server/src/control/Resytor.java
@@ -0,0 +1,17 @@
+package control;
+
+import dao.Dao;
+
+/**
+ *
+ * @author LuizVenturote https://github.com/luizventurote
+ */
+public class Resytor {
+    
+    Dao dao;
+
+    public Resytor() {
+        this.dao = new Dao();
+    }
+
+}
